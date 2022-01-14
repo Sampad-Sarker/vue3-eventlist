876858af6287949375f298ca7056a949e89be524
Sampad Sarker <sampadsarker777@gmail.com>
2022-01-14 11:33:07 +0600

    upload

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventServices from '../services/EventServices'
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+      //id: 123,
+    }
+  },
+  created() {
+    EventServices.getEvent(this.id)
+      .then((res) => (this.event = res.data))
+      .catch((err) => console.log(err))
+  },
+}
+</script>
---
+++ src/views/EventList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="events">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Up Coming Events</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+// import axios from "axios";
+import EventServices from '../services/EventServices.js'
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+
+  data() {
+    return {
+      // events:null,
+      events: [],
+    }
+  },
+
+  created() {
+    EventServices.getEvents()
+      // axios
+      //   .get(
+      //     "https://my-json-server.typicode.com/Sampad-Sarker/apiDatabase/events"
+      //   )
+      .then((res) => {
+        console.log(res)
+        this.events = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
